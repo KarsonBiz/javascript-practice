fc94b9255bc0addc1d4de4844fe3367d948e6ee9
Karson Bizzell <99280654+KarsonBiz@users.noreply.github.com>
2022-08-09 09:03:41 -0500

    Finished Sleep Debt Calculator

--- sleep-debt-calculator/index.js
+++ sleep-debt-calculator/index.js
@@ -1,30 +1,51 @@
 const getSleepHours = day => {
-    if (day === 'Sunday') {
-        return '8';
-    } else if (day === 'Monday') {
-        return '8';
-    } else if (day === 'Tuesday') {
-        return '7.5';
-    } else if (day === 'Wednesday') {
-        return '7';
-    } else if (day === 'Thursday') {
-        return '7';
-    } else if (day === 'Friday') {
-        return '6';
+    if (day === 'monday') {
+      return '8';
+    } else if (day === 'tuesday') {
+      return '8';
+    } else if (day === 'wednesday') {
+      return '7.5';
+    } else if (day === 'thursday') {
+      return '7';
+    } else if (day === 'friday') {
+      return '7';
+    } else if (day === 'saturday') {
+      return '6';
+    } else if (day === 'sunday') {
+      return '8';
     } else {
-        return '6';
+      return "error";
     }
-};
-
-const getActualSleepHours = () => {
-    getSleepHours('Monday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday') + getSleepHours('Sunday')
-};
-
-console.log(getActualSleepHours());
-
-const getIdealSleepHours = () => {
+  };
+  
+  const getActualSleepHours = () => {
+    return (
+    getSleepHours('monday') + 
+    getSleepHours('tuesday') + 
+    getSleepHours('wednesday') + 
+    getSleepHours('thursday') + 
+    getSleepHours('friday') + 
+    getSleepHours('saturday') + 
+    getSleepHours('sunday')
+    );
+  };
+  
+  const getIdealSleepHours = () => {
     const idealHours = 8;
     return idealHours * 7;
-};
-
-console.log(getIdealSleepHours());
+  };
+  
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+  
+    if (actualSleepHours > idealSleepHours) {
+      console.log("You got more sleep than you needed this week. You got" + (actualHoursofSleep - idealSleepHours) + "hours more than you needed.");
+    } else if (actualSleepHours < idealSleepHours) {
+      console.log(`You didn't get enough sleep this week. You should have` + (idealSleepHours - actualSleepHours) + `hours more per week.`);
+    } else {
+      console.log('You got the perfect amount of sleep this week.');
+    };
+  };
+  
+  calculateSleepDebt();
